b90ea8afe681ac94452f4d5d93fd2c53a9af717d
최석영 <sy.c@kt.com>
2026-01-28 16:03:16 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,8 @@ module "virtual_machine" {
   source  = "Azure/avm-res-compute-virtualmachine/azurerm"
   version = "0.18.1"
 
+  count = 0
+
   resource_group_name        = local.resource_group_name
   os_type                    = "linux"
   name                       = local.resource_names.virtual_machine_name
@@ -52,6 +54,7 @@ module "virtual_machine" {
 
   tags = var.tags
 }
+
 
 
 
